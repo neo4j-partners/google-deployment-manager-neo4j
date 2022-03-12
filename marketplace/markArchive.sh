68f3ddaa4273fcc9160b828ae6a30adb15b08c4a
Ben Lackey <ben.lackey@neo4j.com>
2022-03-12 13:34:36 -0800

    first pass

---
+++ marketplace/markArchive.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+license=$1
+rm archive.zip
+mkdir tmp
+
+cp neo4j.py tmp
+cp neo4j.py.display tmp
+cp neo4j.py.schema tmp
+cp c2d_deployment_configuration.json tmp
+cp test_config.yaml tmp
+
+cp ../simple/deployment.py tmp
+cp ../simple/node.sh tmp
+
+cp -r resources tmp
+
+zip -r -X archive.zip tmp
+rm -rf tmp

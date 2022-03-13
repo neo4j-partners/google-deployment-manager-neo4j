870cb91687c7c7e4833cf07636459d0ce980d13a
Ben Lackey <ben.lackey@neo4j.com>
2022-03-13 09:53:59 -0700

    rough structure - this totally doesn't deploy
    Former-commit-id: f23f2692c9c06bb3c26ed4a709c87d51356d5451

---
+++ simple/cluster.py
@@ -0,0 +1,78 @@
+URL_BASE = 'https://www.googleapis.com/compute/v1/projects/'
+
+def GenerateConfig(context):
+    sourceImage = URL_BASE + 'neo4j-public/global/images/neo4j-ee'
+
+    instanceTemplateName = context.env['deployment'] + '-cluster' + '-it'
+    instanceTemplate = {
+        'name': instanceTemplateName,
+        'type': 'compute.v1.instanceTemplate',
+        'properties': {
+            'properties': {
+                'machineType': context.properties['nodeType'],
+                'networkInterfaces': [{
+                    'network': URL_BASE + context.env['project'] + '/global/networks/default',
+                    'accessConfigs': [{
+                        'name': 'External NAT',
+                        'type': 'ONE_TO_ONE_NAT'
+                    }]
+                }],
+                'disks': [{
+                    'deviceName': 'boot',
+                    'type': 'PERSISTENT',
+                    'boot': True,
+                    'autoDelete': True,
+                    'initializeParams': {
+                        'sourceImage': sourceImage
+                    },
+                    'diskType': 'pd-ssd',
+                    'diskSizeGb': context.properties['diskSize']
+                }],
+                'metadata': {'items': [{'key':'startup-script', 'value':GenerateStartupScript(context)}]},
+                'serviceAccounts': [{
+                    'email': 'default',
+                    'scopes': [
+                        'https://www.googleapis.com/auth/cloud-platform',
+                        'https://www.googleapis.com/auth/cloud.useraccounts.readonly',
+                        'https://www.googleapis.com/auth/devstorage.read_only',
+                        'https://www.googleapis.com/auth/logging.write',
+                        'https://www.googleapis.com/auth/monitoring.write',
+                        'https://www.googleapis.com/auth/cloudruntimeconfig'
+                    ]
+                }]
+            }
+        }
+    }
+
+    instanceGroupManager = {
+        'name': context.env['deployment'] + '-cluster' + '-igm',
+        'type': 'compute.v1.regionInstanceGroupManager',
+        'properties': {
+            'region': context.properties['region'],
+            'baseInstanceName': context.env['deployment'] + '-cluster' + '-instance',
+            'instanceTemplate': '$(ref.' + instanceTemplateName + '.selfLink)',
+            'targetSize': context.properties['nodeCount'],
+            'autoHealingPolicies': [{
+                'initialDelaySec': 60
+            }]
+        }
+    }
+
+    config={}
+    config['resources'] = []
+    config['resources'].append(instanceTemplate)
+    config['resources'].append(instanceGroupManager)
+    return config
+
+def GenerateStartupScript(context):
+    script = '#!/usr/bin/env bash\n\n'
+    script += 'DEPLOYMENT="' + context.env['deployment'] + '"\n'
+    script += 'CLUSTER="' + context.properties['cluster'] + '"\n'
+
+    script += 'adminUsername="' + context.properties['adminUsername'] + '"\n'
+    script += 'adminPassword="' + context.properties['adminPassword'] + '"\n'
+
+    script += 'services="' + servicesParameter + '"\n\n'
+    script+= context.imports['server.sh']
+
+    return script

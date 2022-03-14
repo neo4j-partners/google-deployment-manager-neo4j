a63ee45585d913cf4a23b7b0ed0744eb82d7cb49
Ben Lackey <ben.lackey@neo4j.com>
2022-03-13 21:56:21 -0700

    drop unused from dm
    Former-commit-id: 3945662153a8bf8818408d0651c4fc16903cd65e

--- simple/node.sh
+++ simple/node.sh
@@ -55,8 +55,8 @@ if [[ $nodeCount == 1 ]]; then
   echo Running on a single node.
 else
   echo Running on multiple nodes.  Configuring membership in neo4j.conf...
-  coreMembers='vm0X,vm1X,vm2X'
-  coreMembers=$(echo $coreMembers | sed 's/X/.node-'$uniqueString'.'$location'.cloudapp.azure.com:5000/g')
+  coreMembers='10.0.0.2X,10.0.0.3X,10.0.0.4X'
+  coreMembers=$(echo $coreMembers | sed 's/X/:5000/g')
   sed -i s/#causal_clustering.initial_discovery_members=localhost:5000,localhost:5001,localhost:5002/causal_clustering.initial_discovery_members=${coreMembers}/g /etc/neo4j/neo4j.conf
   sed -i s/#dbms.mode=CORE/dbms.mode=CORE/g /etc/neo4j/neo4j.conf
 fi

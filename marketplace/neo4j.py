eb3bb8c2f32e12a0e0d6c7587a6b57768ced92ac
Ben Lackey <ben.lackey@neo4j.com>
2022-03-13 22:50:53 -0700

    drop pw generate
    Former-commit-id: 730eae27264753e6eeca9ce013e42574b44d0e3e

--- marketplace/neo4j.py
+++ marketplace/neo4j.py
@@ -3,30 +3,22 @@ def GenerateConfig(context):
     config['resources'] = []
     config['outputs'] = []
 
-    adminPassword = GeneratePassword()
-
-    config['outputs'].append({
-        'name': 'adminPassword',
-        'value': adminPassword
-    })
-
     deployment = {
         'name': 'deployment',
         'type': 'deployment.py',
         'properties': {
-            'adminPassword': adminPassword
+            'region': context.properties['region'],
+            'nodeCount': context.properties['nodeCount'],
+            'nodeType': context.properties['nodeType'],
+            'diskSize': context.properties['diskSize'],
+            'adminPassword': context.properties['adminPassword'],
+            'graphDatabaseVersion': context.properties['graphDatabaseVersion'],
+            'graphDataScienceVersion': context.properties['graphDataScienceVersion'],
+            'graphDataScienceLicenseKey': context.properties['graphDataScienceLicenseKey'],
+            'bloomVersion': context.properties['bloomVersion'],
+            'bloomLicenseKey': context.properties['bloomLicenseKey']
         }
     }
     config['resources'].append(deployment)
 
     return config
-
-def GeneratePassword():
-    import random
-    categories = ['ABCDEFGHJKLMNPQRSTUVWXYZ', 'abcdefghijkmnopqrstuvwxyz', '123456789', '*-+.']
-    password=[]
-    for category in categories:
-        password.insert(random.randint(0, len(password)), random.choice(category))
-    while len(password) < 8:
-        password.insert(random.randint(0, len(password)), random.choice(''.join(categories)))
-    return ''.join(password)

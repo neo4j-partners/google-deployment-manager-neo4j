b439d1633b5fa61a314e229e3f9d534255b2cd43
Ben Lackey <ben.lackey@neo4j.com>
2022-03-15 11:48:00 -0700

    runtime config to v1
    Former-commit-id: 52bec892c641030699fc391ecd704c550aba524c

--- simple/deployment.py
+++ simple/deployment.py
@@ -2,6 +2,16 @@ def GenerateConfig(context):
     config={}
     config['resources'] = []
 
+    runtimeconfigName = context.env['deployment'] + '-runtimeconfig'
+    runtimeconfig = {
+        'name': runtimeconfigName,
+        'type': 'runtimeconfig.v1.config',
+        'properties': {
+            'config': runtimeconfigName
+        }
+    }
+    config['resources'].append(runtimeconfig)
+
     clusterJSON = {
         'name': context.env['deployment'] + '-cluster',
         'type': 'cluster.py',

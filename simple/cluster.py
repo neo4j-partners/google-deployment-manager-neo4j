94ae0713debba56b08485d760410fb68f72cbe20
Ollie Hughes <oliver.hughes@neotechnology.com>
2022-11-15 18:46:38 +0000

    Conditionally create loadbalancer when instances > 1
    Former-commit-id: 714bc5ccf6cd1c3b64677955b9380fc749636bb3

--- simple/cluster.py
+++ simple/cluster.py
@@ -6,100 +6,14 @@ def GenerateConfig(context):
     healthcheck_http_name = prefix + '-http-healthcheck'
     forwardingrule_name = prefix + '-http-forwardingrule'
     loadbalancer_name = prefix + '-http-loadbalancer'
-    instance_template_name = prefix + '-cluster' + '-it'
+    instance_template_name = prefix + '-cluster-it'
+    standalone_instance_template_name = prefix + '-standalone-it'
+    public_ip_name = prefix + '-standalone-ip'
     igm_name = prefix + '-cluster' + '-igm'
+    standalone_igm_name = prefix + '-standalone' + '-igm'
     startup_waiter_name = prefix + '-startup-waiter'
     startup_config_name = prefix + '-startup-config'
 
-    instanceTemplate = {
-        'name': instance_template_name,
-        'type': 'compute.v1.instanceTemplate',
-        'properties': {
-            'properties': {
-                'machineType': context.properties['nodeType'],
-                'tags': {
-                    'items': [prefix + '-external', prefix + '-internal'],
-                },
-                'networkInterfaces': [{
-                    'network': 'https://www.googleapis.com/compute/v1/projects/' + context.env['project'] + '/global/networks/default',
-                    'accessConfigs': [{
-                        'name': 'External NAT',
-                        'type': 'ONE_TO_ONE_NAT'
-                    }]
-                }],
-                'disks': [{
-                    'deviceName': 'boot',
-                    'type': 'PERSISTENT',
-                    'boot': True,
-                    'autoDelete': True,
-                    'initializeParams': {
-                        'sourceImage': sourceImage
-                    },
-                    'diskType': 'pd-ssd',
-                    'diskSizeGb': context.properties['diskSize']
-                }],
-                'metadata': {'items': [{'key':'startup-script', 'value': GenerateStartupScript(context)}]},
-                'serviceAccounts': [{
-                    'email': 'default',
-                    'scopes': [
-                        'https://www.googleapis.com/auth/cloud-platform',
-                        'https://www.googleapis.com/auth/cloud.useraccounts.readonly',
-                        'https://www.googleapis.com/auth/devstorage.read_only',
-                        'https://www.googleapis.com/auth/logging.write',
-                        'https://www.googleapis.com/auth/monitoring.write',
-                        'https://www.googleapis.com/auth/cloudruntimeconfig'
-                    ]
-                }],
-                'labels': {
-                   'goog-dm': context.env['deployment']
-                }
-            }
-        }
-    }
-
-    instanceGroupManager = {
-        'name': igm_name,
-        'type': 'compute.v1.regionInstanceGroupManager',
-        'properties': {
-            'region': context.properties['region'],
-            'baseInstanceName': context.env['deployment'] + '-cluster' + '-instance',
-            'instanceTemplate': '$(ref.' + instance_template_name + '.selfLink)',
-            'targetSize': context.properties['nodeCount']
-        }
-    }
-    healthcheck_http = {
-        'name': healthcheck_http_name,
-        'type': 'compute.v1.regionHealthChecks',
-        'properties': {
-            'region': properties['region'],
-            'type': 'TCP',
-            'tcpHealthCheck': {
-                'port': 7474
-            },
-        }
-    }
-
-    loadbalancer = {
-        'name': loadbalancer_name,
-        'type': 'compute.v1.regionBackendService',
-        'properties': {
-            'region': properties['region'],
-            'healthChecks': ['$(ref.' + healthcheck_http_name + '.selfLink)'],
-            'backends': [{'group': '$(ref.' + igm_name + '.instanceGroup)'}],
-            'protocol': 'TCP',
-            'loadBalancingScheme': 'EXTERNAL',
-        }
-    }
-    forwarding_rule = {
-        'name': forwardingrule_name,
-        'type': 'compute.v1.forwardingRule',
-        'properties': {
-            'ports': [7474, 7687],
-            'region': properties['region'],
-            'backendService': '$(ref.' + loadbalancer_name + '.selfLink)',
-            'loadBalancingScheme': 'EXTERNAL',
-        }
-    }
     startup_config = {
         'name': startup_config_name,
         'type': 'runtimeconfig.v1beta1.config',
@@ -129,23 +43,159 @@ def GenerateConfig(context):
         }
     }
     config = {'resources': []}
-    config['resources'].append(healthcheck_http)
-    config['resources'].append(loadbalancer)
-    config['resources'].append(forwarding_rule)
-    config['resources'].append(instanceTemplate)
-    config['resources'].append(instanceGroupManager)
     config['resources'].append(startup_config)
     config['resources'].append(startup_waiter)
-
     config['outputs'] = []
-    config['outputs'].append({
-        'name': 'ip',
-        'value': '$(ref.' + forwardingrule_name + '.IPAddress)'
-    })
+
+    if context.properties['nodeCount'] > 1:
+
+        instance_template = {
+            'name': instance_template_name,
+            'type': 'compute.v1.instanceTemplate',
+            'properties': instance_properties(context, prefix, sourceImage)
+        }
+
+        instance_group_manager = {
+            'name': igm_name,
+            'type': 'compute.v1.regionInstanceGroupManager',
+            'properties': {
+                'region': context.properties['region'],
+                'baseInstanceName': context.env['deployment'] + '-cluster' + '-instance',
+                'instanceTemplate': '$(ref.' + instance_template_name + '.selfLink)',
+                'targetSize': context.properties['nodeCount']
+            }
+        }
+        healthcheck_http = {
+            'name': healthcheck_http_name,
+            'type': 'compute.v1.regionHealthChecks',
+            'properties': {
+                'region': properties['region'],
+                'type': 'TCP',
+                'tcpHealthCheck': {
+                    'port': 7474
+                },
+            }
+        }
+
+        loadbalancer = {
+            'name': loadbalancer_name,
+            'type': 'compute.v1.regionBackendService',
+            'properties': {
+                'region': properties['region'],
+                'healthChecks': ['$(ref.' + healthcheck_http_name + '.selfLink)'],
+                'backends': [{'group': '$(ref.' + igm_name + '.instanceGroup)'}],
+                'protocol': 'TCP',
+                'loadBalancingScheme': 'EXTERNAL',
+            }
+        }
+        forwarding_rule = {
+            'name': forwardingrule_name,
+            'type': 'compute.v1.forwardingRule',
+            'properties': {
+                'ports': [7474, 7687],
+                'region': properties['region'],
+                'backendService': '$(ref.' + loadbalancer_name + '.selfLink)',
+                'loadBalancingScheme': 'EXTERNAL',
+            }
+        }
+        config['resources'].append(instance_template)
+        config['resources'].append(instance_group_manager)
+        config['resources'].append(healthcheck_http)
+        config['resources'].append(loadbalancer)
+        config['resources'].append(forwarding_rule)
+
+        config['outputs'].append({
+            'name': 'ip',
+            'value': '$(ref.' + forwardingrule_name + '.IPAddress)'
+        })
+
+    else:
+        public_ip_addresss = {
+            'name': public_ip_name,
+            'type': 'compute.v1.address',
+            'properties': {
+                'region': context.properties['region']
+            }
+        }
+        instance_template = {
+            'name': standalone_instance_template_name,
+            'type': 'compute.v1.instanceTemplate',
+            'metadata': {
+                'dependsOn': [public_ip_name]
+            },
+            'properties': instance_properties(context, prefix, sourceImage, '$(ref.' + public_ip_name + '.address)')
+        }
+
+        instance_group_manager = {
+            'name': standalone_igm_name,
+            'type': 'compute.v1.regionInstanceGroupManager',
+            'metadata': {
+                'dependsOn': [public_ip_name]
+            },
+            'properties': {
+                'region': context.properties['region'],
+                'baseInstanceName': context.env['deployment'] + '-cluster' + '-instance',
+                'instanceTemplate': '$(ref.' + standalone_instance_template_name + '.selfLink)',
+                'targetSize': context.properties['nodeCount']
+            }
+        }
+        config['resources'].append(public_ip_addresss)
+        config['resources'].append(instance_template)
+        config['resources'].append(instance_group_manager)
+        config['outputs'].append({
+            'name': 'ip',
+            'value': '$(ref.' + public_ip_name + '.address)'
+        })
     return config
 
 
-def GenerateStartupScript(context):
+def instance_properties(context, prefix, sourceImage, public_ip=None):
+    properties = {'properties': {
+        'machineType': context.properties['nodeType'],
+        'tags': {
+            'items': [prefix + '-external', prefix + '-internal'],
+        },
+        'networkInterfaces': [{
+            'network': 'https://www.googleapis.com/compute/v1/projects/' + context.env[
+                'project'] + '/global/networks/default',
+            'accessConfigs': [{
+                'name': 'External NAT',
+                'type': 'ONE_TO_ONE_NAT'
+            }]
+        }],
+        'disks': [{
+            'deviceName': 'boot',
+            'type': 'PERSISTENT',
+            'boot': True,
+            'autoDelete': True,
+            'initializeParams': {
+                'sourceImage': sourceImage
+            },
+            'diskType': 'pd-ssd',
+            'diskSizeGb': context.properties['diskSize']
+        }],
+        'metadata': {'items': [{'key': 'startup-script', 'value': generate_startup_script(context)}]},
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/cloud-platform',
+                'https://www.googleapis.com/auth/cloud.useraccounts.readonly',
+                'https://www.googleapis.com/auth/devstorage.read_only',
+                'https://www.googleapis.com/auth/logging.write',
+                'https://www.googleapis.com/auth/monitoring.write',
+                'https://www.googleapis.com/auth/cloudruntimeconfig'
+            ]
+        }],
+        'labels': {
+            'goog-dm': context.env['deployment']
+        }
+    }}
+    if public_ip:
+        properties['properties']['networkInterfaces'][0]['accessConfigs'][0]['natIP'] = public_ip
+    return properties
+
+
+def generate_startup_script(context):
     script = '#!/usr/bin/env bash\n\n'
 
     script += 'deployment="' + context.env['deployment'] + '"\n'
